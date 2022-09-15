95530e9644adcaa4f540c4a368af2a09cea34f8e
parkinso <89875845827@mail.ru>
2022-09-15 18:08:18 +0300

    addHttpUnitTests

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -31,24 +31,32 @@ func (h *handler) Register(router *httprouter.Router) {
 
 func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	w.Write([]byte("this is list of users "))
+	w.WriteHeader(200)
 }
 
 func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(201)
 	w.Write([]byte("this is create user"))
+
 }
 
 func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
 	w.Write([]byte("this is get user by UUID"))
+
 }
 
 func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
 	w.Write([]byte("this is update user"))
 }
 
 func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
 	w.Write([]byte("this is partially update user "))
 }
 
 func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
 	w.Write([]byte("this is delete user"))
 }
