cd450fd781467c729dbb44cb2065e8c7ff0a90bb
parkinso <89875845827@mail.ru>
2022-09-15 15:00:53 +0300

    start-Server

---
+++ cmd/web/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net"
+	"net/http"
+	"time"
+)
+
+func IndexHundler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	name := params.ByName("name")
+	w.Write([]byte(fmt.Sprintf("Hello %s", name)))
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/:name", IndexHundler)
+
+	fmt.Println("Starting server...")
+
+	listener, err := net.Listen("tcp", "127.0.0.1:1234")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Sucess")
+	server := &http.Server{
+		Handler:      router,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatalln(server.Serve(listener))
+
+}
