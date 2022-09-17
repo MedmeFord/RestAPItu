52b7abf59f740051175d7ea8bf937b8d6dd10fd4
parkinso <89875845827@mail.ru>
2022-09-17 11:29:05 +0300

    FixBagPermisionDeniedForDirecry

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -9,24 +9,25 @@ import (
 	"runtime"
 )
 
-type writerHook struct {
-	Writer   []io.Writer
-	LogLevel []logrus.Level
+type writeHook struct {
+	Writer    []io.Writer
+	LogLevels []logrus.Level
 }
 
-func (hook *writerHook) Fire(entry *logrus.Entry) error {
+func (hook *writeHook) Fire(entry *logrus.Entry) error {
 	line, err := entry.String()
 	if err != nil {
 		return err
 	}
+
 	for _, w := range hook.Writer {
 		w.Write([]byte(line))
 	}
 	return err
 }
 
-func (hook writerHook) Levels() []logrus.Level {
-	return hook.LogLevel
+func (hook *writeHook) Levels() []logrus.Level {
+	return hook.LogLevels
 }
 
 var e *logrus.Entry
@@ -48,28 +49,28 @@ func init() {
 	l.SetReportCaller(true)
 	l.Formatter = &logrus.TextFormatter{
 		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
-			fileName := path.Base(frame.File)
-			return fmt.Sprintf("%s()", frame.Function), fmt.Sprintf("%s:%d", fileName, frame.Line)
+			filename := path.Base(frame.File)
+			return fmt.Sprintf("%s()", frame.Function), fmt.Sprintf("%s:%d", filename, frame.Line)
 		},
 		DisableColors: false,
 		FullTimestamp: true,
 	}
 
-	err := os.MkdirAll("logs", 0644)
+	err := os.MkdirAll("logging/logs", 6640) // создает папку
 	if err != nil {
 		panic(err)
 	}
 
-	allFile, err := os.OpenFile("logs/all.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0640)
+	allFile, err := os.OpenFile("logging/logs/all.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 6640) // открывает и записывает
 	if err != nil {
 		panic(err)
 	}
 
-	l.SetOutput(io.Discard)
+	l.SetOutput(io.Discard) // что бы логи по умолчанию не куда не уходили
 
-	l.AddHook(&writerHook{
-		Writer:   []io.Writer{allFile, os.Stdout},
-		LogLevel: logrus.AllLevels,
+	l.AddHook(&writeHook{
+		Writer:    []io.Writer{allFile, os.Stdout},
+		LogLevels: logrus.AllLevels,
 	})
 
 	l.SetLevel(logrus.TraceLevel)
